559bc47c8b0e9505ada9fcbdec51ad995c19d122
Nelson Gustavo <nelsongborges@gmail.com>
2016-02-07 13:28:47 -0200

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,37 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require js/jquery-1.11.1.min.js
+
+//= require turbolinks
+
+//= require js/pace/pace.min.js
+//= require js/bootstrap/bootstrap.js
+//= require js/modernizr/modernizr.js
+//= require js/devicejs/device.js
+//= require js/tinynav/tinynav.min.js
+//= require js/smoothscroll/jquery.smooth-scroll.js
+//= require js/flexslider/jquery.flexslider.js
+//= require js/sticky/jquery.sticky.js
+//= require js/waypoint/jquery.waypoints.min.js
+//= require js/jquery-ui-widget/jquery.ui.widget.js
+//= require js/jquery-doubletaptogo/jquery.dcd.doubletaptogo.js
+//= require js/vide/jquery.vide.js
+//= require js/stellar/jquery.stellar.js
+//= require js/countdown/jquery.countdown.js
+//= require js/magnific-popup/jquery.magnific-popup.js
+//= require js/owlcarousel/owl.carousel.js
+//= require js/masonry/masonry.pkgd.min.js
+//= require js/rsvp/rsvp.js
+//= require js/script.js
+//= require js/map-script.js
+//= require js/main-slider-fade.js
