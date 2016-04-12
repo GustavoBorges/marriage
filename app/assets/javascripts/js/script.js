4e8e337ed74dcbcd69e31a970b16b415ee70bbf2
Nelson Gustavo <nelsongborges@gmail.com>
2016-04-11 21:02:54 -0300

    ajustando data

--- app/assets/javascripts/js/script.js
+++ app/assets/javascripts/js/script.js
@@ -336,7 +336,7 @@ $(document).ready(function() {
 	// 07. COUNTDOWN
 	//===================================================================================
 	var theday = new Date();
-	theday = new Date(2016, 5, 14);
+	theday = new Date(2016, 4, 14);
 	$('#countdown').countdown({until: theday, format: 'WDHMS'});
 	$('#countdown').countdown($.countdown.regionalOptions['custom-label']); 
 		 
